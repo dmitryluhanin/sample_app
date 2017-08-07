7b7e9ff7ce4f1bc35d729ec0a36509c1b5386881
dmitryluhanin <dmitry.luhanin@gmail.com>
2017-08-07 11:22:05 +0300

    [ci skip]

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Copy bash script"
-scp ./deploy_host.sh $DEPLOY_USER@$DEPLOY_HOST:/root/deploy_host.sh
+scp ./deploy_host.sh $DEPLOY_USER@$DEPLOY_HOST:/home/$DEPLOY_USER/deploy_host.sh
 echo "Run bash script"
 ssh $DEPLOY_USER@$DEPLOY_HOST  '/bin/sh /root/deploy_host.sh  $DOCKER_EMAIL $DOCKER_USER $DOCKER_PASS'
 sleep 10
